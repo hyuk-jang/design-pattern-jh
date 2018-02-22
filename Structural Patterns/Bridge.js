c9099583a7c000ec3649e892ecc5f815a1ef6677
장혁 <hyuk.jang@smsoft.co.kr>
2018-02-22 17:59:35 +0900

    edit

--- Structural Patterns/Bridge.js
+++ Structural Patterns/Bridge.js
@@ -1,4 +1,6 @@
-// 구현 뿐만 아니라 추상화된 부분까지 변경시켜야 하는 경우
+// bridge는 '무엇인가를 연결한다'는 의미이다. bridge 패턴도 두 장소를 연결하는 역할을 한다. bridge 패턴은 기능의 클래스 계층과 구현의 클래스 계층을 연결하고, 구현부에서 추상 계층을 분리하여 각자 독립적으로 변형할 수 있게 해준다.
+
+// [그림 5-37]에서 supperClass의 method()는 implementor의 impMethod()를 사용해 구현되고, subClass의 subMethod()는 supperClass의 method()들을 사용해 구현된다. 그 결과 subClass와 implementor(concreteImplement)가 분리된다. bridge 패턴은 구현과 인터페이스(추상화된 부분)를 분리할 수 있고, 추상화된 부분과 실제 구현 부분을 독립적으로 확장할 수 있다.
 
 'use strict';
 
--- Structural Patterns/Flyweight.js
+++ Structural Patterns/Flyweight.js
@@ -1,4 +1,8 @@
-// 어떤 클래스의 인스턴스 한 개만가지고 여러 개의 "가상 인스턴스"를 제공하고 싶을 때
+// flyweight는 '(권투·레슬링 등의) 플라이급 선수(보통 체중 48~51kg 사이)'를 말한다. 즉 가벼운 것이다. 그렇다면 패턴에서 flyweight는 무엇이 가볍다는 것일까? 일반적으로 메모리 사용량을 말할 때 많이 사용하면 무겁다고 하고, 적게 사용하면 가볍다고 한다. 따라서 flyweight 패턴은 메모리를 가볍게 해준다고 짐작할 수 있다. 자바에서 인스턴스를 생성할 때는 new를 사용한다.
+
+// 인스턴스를 많이 생성한다면 new가 많아지고 이는 메모리 사용량이 많아짐을 의미한다. flyweight 패턴은 메모리 사용량을 줄이기 위한 방법으로, 인스턴스를 필요한 대로 다 만들어 쓰지 말고, 동일한 것은 가능하면 공유해서 객체 생성을 줄이자는 것이다.
+
+// flyweight 패턴의 구조를 [그림 5-41]에서 살펴보면 클래스별로 factory를 만들고(flyweightFactory), 그 factory(flyweightFactory)에서는 자신이 생성한 객체들을 관리한다. 만일 클라이언트가 요청한 객체를 factory가 가지고 있으면(concreteFlyweight) 그것을 넘겨주면 된다. 그런데 요청한 객체가 factory에 없으면(unsharedConcreteFlyweight) 새로 객체를 만들어 저장해놓고 그것을 넘겨준다.
 
 'use strict';
 
@@ -58,6 +62,7 @@ class UnsharedConcreteFlyweight extends Flyweight {
     for(var i = 0; i < extrinsicState; i++) {
       //random key
       key = this.keys[Math.floor(Math.random() * (this.keys.length))];
+      console.log('key', key);
       word = this.flyweights.GetFlyweight(key).Operation(word);
     }
     console.log('UnsharedConcreteFlyweight Operation: ');
@@ -67,8 +72,10 @@ class UnsharedConcreteFlyweight extends Flyweight {
 
 function init_Flyweight() {
   var flyweights = new FlyweightFactory();
-  var gibberish = flyweights.CreateGibberish(['-', '+', '*']);
+  var gibberish = flyweights.CreateGibberish(['고양이', '사자', '여우']);
+  console.dir(gibberish);
   gibberish.Operation(5);
-  gibberish.Operation(10);
+  console.dir(gibberish);
+  // gibberish.Operation(10);
 }
 init_Flyweight();
